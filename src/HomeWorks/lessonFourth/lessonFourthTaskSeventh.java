a7f3e1d7b5269eb41f44473262a1d9ae9c02a296
yaroslavomelyanenko <omelianenko.yaroslav@gmail.com>
2018-12-27 15:31:03 +0200

    commit 1

--- src/HomeWorks/lessonFourth/lessonFourthTaskSeventh.java
+++ src/HomeWorks/lessonFourth/lessonFourthTaskSeventh.java
@@ -4,13 +4,16 @@ public class lessonFourthTaskSeventh {
     //Найти в массиве число, которое повторяется наибольшее количество раз. Не использовать коллекции.
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 3, 4, 6, 1, 6, 6, 6};
-        int res = 0, cc, cp = 0;
-        for (int i = 0; i < a.length; i++) { cc = 0;
+        int num = 0;
+        int count = 0;
+        int countTemp = 0;
+        for (int i = 0; i < a.length; i++) {
+            //cc = 0;
             for (int j = 0; j < a.length; j++) { if (a[j] == a[i])
-                cc++; }
-            if (cc > cp) { cp = cc;
-                res = a[i]; }
+                countTemp++; }
+            if (countTemp > count) { num = a[i];
+                count = countTemp; }
         }
-        System.out.println(res);
+        System.out.println(num);
     }
 }
