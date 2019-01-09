362620a931b7188a53b27c8c92981d073d5cbbf5
yaroslavomelyanenko <omelianenko.yaroslav@gmail.com>
2019-01-09 14:41:35 +0200

    commit 1

--- src/HomeWorks/lessonFifth/lessonFifthTaskThird.java
+++ src/HomeWorks/lessonFifth/lessonFifthTaskThird.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 //Ввести с консоли целое число. Посчитать количество единиц в его бинарном представлении (“1011” –> 3).
 public class lessonFifthTaskThird {
 
-    public static int a;
-    public static int sum = 0;
-
     public static void main(String[] args) {
         bin();
-        showSum();
+        //showSum();
     }
-
-    public static String bin(){
+    static String bin() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите Ваше число");
         int a = sc.nextInt();
         String b = Integer.toBinaryString(a);
         System.out.println("Ваше число в бинарном представлеии будет " + b);
-        return b;
+        return showSum(b);
     }
-
-    public static void showSum() {
-        for (int i = 0; i < b.length(); i++){
+    private static String showSum(String b) {
+        int sum = 0;
+        for (int i = 0; i < b.length(); i++) {
             if (b.charAt(i) == '1') sum++;
-            System.out.println("Содержит = " + sum + " символов '1' ");
         }
+        System.out.println("Содержит = " + sum + " символов '1' ");
+        return b;
     }
 }
